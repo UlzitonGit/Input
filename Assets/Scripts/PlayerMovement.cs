0e7d58796783822f29448cb1e93a63ffb671af35
ITHUB\Teacher <312>
2025-02-04 11:04:29 +0300

    Deb

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CapsuleCollider))]
@@ -11,11 +12,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _jumpSpeed = 5f;
 
     private Rigidbody _rb;
+    
     private CapsuleCollider _capsule;
     private Vector2 _playerMovement;
     // Start is called before the first frame update
     private void OnValidate()
     {
+        
         _rb = GetComponent<Rigidbody>();
         _capsule = GetComponent<CapsuleCollider>();
     }
@@ -33,9 +36,11 @@ public class PlayerMovement : MonoBehaviour
     {
         _playerMovement = new Vector2(_input.MoveInput.x, _input.MoveInput.y);
         _rb.velocity = new Vector2(_playerMovement.x * _speed, _rb.velocity.y);
+        transform.DORotateQuaternion(Quaternion.Euler(transform.rotation.x, transform.rotation.y + _input.MoveInput.x * 90, transform.rotation.z), 1);
+        
     }
     private void Jump()
     {
-        _rb.AddForce(transform.up * _jumpSpeed, ForceMode.Impulse);
+        transform.DOJump(transform.up * 5, 2, 1, 2);
     }
 }
